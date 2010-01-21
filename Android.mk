56ba3047b745a899d6efbc6fecf2e378fadba460
Friedrich Schäuffelhut <fries@desert.lnp.org>
2010-01-21 18:07:16 +0100

    Added a target producing a statically linked binary (openvpn-static).

--- Android.mk
+++ Android.mk
@@ -1,18 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
 
-#ifeq ($(TARGET_ARCH),arm)
-#	LOCAL_CFLAGS += -DOPENSSL_BN_ASM_MONT -DAES_ASM -DSHA1_ASM -DSHA256_ASM -DSHA512_ASM
-#	LOCAL_SRC_FILES:= 0.9.9-dev/bn/armv4-mont.s \
-#	                  0.9.9-dev/aes/aes-armv4.s \
-#	                  0.9.9-dev/sha/sha1-armv4-large.s \
-#	                  0.9.9-dev/sha/sha256-armv4.s \
-#	                  0.9.9-dev/sha/sha512-armv4.s
-#else
-#	LOCAL_SRC_FILES:= aes/aes_core.c
-#endif
-
-LOCAL_SRC_FILES:= \
+common_SRC_FILES:= \
 	base64.c base64.h \
 	basic.h \
 	buffer.c buffer.h \
@@ -79,28 +67,51 @@ LOCAL_SRC_FILES:= \
 	cryptoapi.h cryptoapi.c\
 	rsa_depr.c
 
-#LOCAL_CFLAGS += -DNO_WINDOWS_BRAINDEATH 
+#common_CFLAGS += -DNO_WINDOWS_BRAINDEATH 
 
-#include $(LOCAL_PATH)/../android-config.mk
-
-LOCAL_C_INCLUDES += \
+common_C_INCLUDES += \
 	external/openssl \
 	external/openssl/include \
 	external/openssl/crypto \
 	external/lzo/include
 
-# LOCAL_SHARED_LIBRARIES += libengines
-LOCAL_SHARED_LIBRARIES += libcrypto libssl liblzo
+# common_SHARED_LIBRARIES += libengines
 
 ifneq ($(TARGET_SIMULATOR),true)
-	LOCAL_SHARED_LIBRARIES += libdl
+	common_SHARED_LIBRARIES += libdl
 endif
 
-#LOCAL_LDLIBS += -ldl -lcrypto -lssl
-LOCAL_LDLIBS += -ldl
+# static linked binary
+# =====================================================
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= $(common_SRC_FILES)
+LOCAL_CFLAGS:= $(common_CFLAGS)
+LOCAL_C_INCLUDES:= $(common_C_INCLUDES)
+
+LOCAL_SHARED_LIBRARIES += $(common_SHARED_LIBRARIES)
+LOCAL_STATIC_LIBRARIES:= libopenssl-static liblzo-static
+
+#LOCAL_LDLIBS += -ldl
+#LOCAL_PRELINK_MODULE:= false
+
+LOCAL_MODULE:= openvpn-static
+LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
+include $(BUILD_EXECUTABLE)
+
+# dynamic linked binary
+# =====================================================
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES:= $(common_SRC_FILES)
+LOCAL_CFLAGS:= $(common_CFLAGS)
+LOCAL_C_INCLUDES:= $(common_C_INCLUDES)
+
+LOCAL_SHARED_LIBRARIES:= $(common_SHARED_LIBRARIES) libssl libcrypto liblzo
+
+#LOCAL_LDLIBS += -ldl
+#LOCAL_PRELINK_MODULE:= false
 
 LOCAL_MODULE:= openvpn
 LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
-
 include $(BUILD_EXECUTABLE)
-#include $(BUILD_SHARED_LIBRARY)
